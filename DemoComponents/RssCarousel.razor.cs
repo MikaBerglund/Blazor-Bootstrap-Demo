53603d7078815133313b0258e17b5eaa555356d5
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-16 11:19:45 +0200

    Completed the RSS carousel.

--- DemoComponents/RssCarousel.razor.cs
+++ DemoComponents/RssCarousel.razor.cs
@@ -17,6 +17,10 @@ namespace DemoComponents
         }
 
 
+
+        [Parameter]
+        public EventCallback<RssCarousel> OnItemsChanged { get; set; }
+
         [Parameter]
         public ICollection<WebPageMetadata> Items { get; set; }
 
@@ -40,7 +44,7 @@ namespace DemoComponents
         private string CurrentUrl;
         protected override void OnParametersSet()
         {
-            if(this.FeedUrl != this.CurrentUrl)
+            if (this.FeedUrl != this.CurrentUrl)
             {
                 this.CurrentUrl = this.FeedUrl;
                 this.LoadItemsAsync(this.CurrentUrl);
@@ -53,7 +57,10 @@ namespace DemoComponents
         private async Task LoadItemsAsync(string url)
         {
             this.ResetItems();
+
             var feed = await Feed.LoadAsync(url);
+            await this.LogInfoAsync("Feed loaded", url, feed);
+
             foreach(var item in feed.Items)
             {
                 if (this.CurrentUrl == this.FeedUrl)
@@ -61,6 +68,7 @@ namespace DemoComponents
                     var meta = await HtmlUtility.ParseMetadataAsync(new Uri(item.Url));
                     this.Items.Add(meta);
                     this.StateHasChanged();
+                    await this.OnItemsChanged.InvokeAsync(this);
                 }
             }
         }
